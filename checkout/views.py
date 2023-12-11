fdda2624eb7dda030c2f8e8be85f01c037da3a00
makiaveli1 <itsmakiaveli@gmail.com>
2023-12-11 11:14:53 +0000

    added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_live_51N09VeLjvedDFbO8shHRxoFCPfbCGPipZMfogFu0eNAbwxqoj9MCStZZwp9wejrk8hsdTUfHrM89D1sFu0KiaQ5W00kFcN8RDk',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
